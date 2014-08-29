c8320889e59dba050e430b82b381da6ecaf88c17
John Whitworth <johnwhit@icloud.com>
2014-08-29 11:12:15 -0700

    Import of SafeQueue Header Implementation
    C++ header file contains the implementation for a thread safe queue.

---
+++ SafeQueue.h
@@ -0,0 +1,59 @@
+/* SafeQueue.h
+ * -----------
+ * Thread safe queue based on Windows C++/CLI libraries. Designed
+ * to be used as a producer/consumer queue.
+ *
+ * Created by John Whitworth on 6/30/14.
+ */
+
+#pragma once
+
+#include <queue>
+#include <windows.h>
+
+template <typename T> class SafeQueue
+{
+private:
+    std::queue<T> queue;
+    CRITICAL_SECTION CritSection;
+    CONDITION_VARIABLE ConditionVar;
+    
+public:
+	SafeQueue(void) {
+		InitializeCriticalSection(&CritSection);
+		InitializeConditionVariable(&ConditionVar);
+	}
+
+	~SafeQueue(void) {
+		DeleteCriticalSection(&CritSection);
+	}
+
+    void add(T item) {
+        EnterCriticalSection(&CritSection);
+        queue.push(item);
+        LeaveCriticalSection(&CritSection);
+        WakeAllConditionVariable(&ConditionVar);
+    }
+    
+    T get() {
+        EnterCriticalSection(&CritSection);
+        if (queue.empty()) {
+            SleepConditionVariableCS(&ConditionVar, &CritSection, INFINITE);
+        }
+        T item = queue.front();
+        queue.pop();
+		LeaveCriticalSection(&CritSection);
+        return item;
+    }
+    
+    int size() {
+        EnterCriticalSection(&CritSection);
+        int size = queue.size();
+        LeaveCriticalSection(&CritSection);
+        return size;
+    }
+
+	bool empty() {
+		return (size() == 0);
+	}
+};
